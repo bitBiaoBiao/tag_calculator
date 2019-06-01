8b02d3ce9e0e97e67151e50660b4d2f2d24927c6
bitBiaoBiao <48502892+bitBiaoBiao@users.noreply.github.com>
2019-06-01 17:14:53 +0800

    add main method

---
+++ src/main/java/com/mip/roaring/tagcalc/TagcalcApplication.java
@@ -0,0 +1,13 @@
+package com.mip.roaring.tagcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagcalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagcalcApplication.class, args);
+	}
+
+}
